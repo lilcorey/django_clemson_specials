33b81d3b917e2942b15e85af1d173b485f1dfe66
Corey Edelman <littlecorey10@aol.com>
2017-05-31 17:06:03 -0400

    index update

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('adais.urls', namespace="adais")),
     url(r'^adais/', include('adais.urls', namespace="adais")),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
